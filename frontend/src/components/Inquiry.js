55fcc6d0e70d8ebc05bde5b05e4f64e914664051
Krzysztof Olszewski <hello@retryjoin.pl>
2023-07-05 17:20:10 +0200

    small fix

--- frontend/src/components/Inquiry.js
+++ frontend/src/components/Inquiry.js
@@ -82,38 +82,38 @@ export default function SignInSide() {
       console.log(inquiryData),
       setSuccessMessage(true),
     )
-    .catch(err => console.log(err));
+      .catch(err => console.log(err));
   };
 
   const isEmpty = (value) => {
     return value == null || value.trim() === "" || value === NaN;
   };
-  const resetForm = (e) => {reset(); setSuccessMessage(false)}
+  const resetForm = (e) => { reset(); setSuccessMessage(false) }
   const theme = useTheme();
 
   return (
     <div id="inquiry">
-    <Box
-      maxWidth={{ sm: 720, md: 1236 }}
-      width={1}
-      margin='0 auto'
-      paddingX={2}
-      paddingY={{ xs: 4, sm: 6, md: 8 }}
-    >
-      <Box marginBottom={4}>
-        <Typography
-          variant='h4'
-          align='center'
-          data-aos='fade-up'
-          fontWeight={700}
-          gutterBottom
-        >
-          Jak możemy Ci pomóc?
-        </Typography>
-      </Box>
-      <Grid container component="main" sx={{ height: {xs: 820, sm: "65vh", md: "65vh"} }}>
-        <CssBaseline />
-        {/* <Box 
+      <Box
+        maxWidth={{ sm: 720, md: 1236 }}
+        width={1}
+        margin='0 auto'
+        paddingX={2}
+        paddingY={{ xs: 4, sm: 6, md: 8 }}
+      >
+        <Box marginBottom={4}>
+          <Typography
+            variant='h4'
+            align='center'
+            data-aos='fade-up'
+            fontWeight={700}
+            gutterBottom
+          >
+            Jak możemy Ci pomóc?
+          </Typography>
+        </Box>
+        <Grid container component="main" sx={{ height: { xs: 820, sm: "65vh", md: "65vh" } }}>
+          <CssBaseline />
+          {/* <Box 
             component={LazyLoadImage}
             src='/images/keenys_048.jpg'
             alt='Background Image'
@@ -123,175 +123,175 @@ export default function SignInSide() {
             width={1}
             maxWidth={1}
             /> */}
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: "url(../../../images/keenys_048.jpg)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
             sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+              backgroundImage: "url(../../../images/keenys_048.jpg)",
+              backgroundRepeat: "no-repeat",
+              backgroundColor: (t) =>
+                t.palette.mode === "light"
+                  ? t.palette.grey[50]
+                  : t.palette.grey[900],
+              backgroundSize: "cover",
+              backgroundPosition: "center",
             }}
-          >
-            {successMessage ?
-              (
-              <Grid item xs={12}>
-              <Typography component="h1" variant="h5">
-                Dziękujemy!, wkrótce skontaktujemy się z Tobą.
-              </Typography>
-              <Button
-              fullWidth
-              variant="outlined"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={resetForm}
-              >
-                Nowa wiadomość
-              </Button>
-              </Grid>
-              ) : (
-                <Grid item xs={12}>
-                  <Typography component="h1" variant="h5" align="center">
-                   Napisz do nas:
-                  </Typography>
-                <Box
-                  component="form"
-                  noValidate
-                  onSubmit={handleSubmit(onSubmit)}
-                  sx={{ mt: 3 }}
-                >
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                      <TextField
-                        autoComplete="given-name"
-                        name="firstName"
-                        required
-                        fullWidth
-                        id="firstName"
-                        label="First Name"
-                        autoFocus
-                        {...register('firstName')}
-                        error={errors.firstName ? true : false}
-                      />
-                      <Typography variant="inherit" color="textSecondary">
-                        {errors.firstName?.message}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <TextField
-                        required
-                        fullWidth
-                        id="lastName"
-                        label="Last Name"
-                        name="lastName"
-                        autoComplete="family-name"
-                        {...register('lastName')}
-                        error={errors.lastName ? true : false}
-                      />
-                      <Typography variant="inherit" color="textSecondary">
-                        {errors.lastName?.message}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        required
-                        fullWidth
-                        id="email"
-                        type="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        {...register('email')}
-                        error={errors.email ? true : false}
-                      />
-                      <Typography variant="inherit" color="textSecondary">
-                        {errors.email?.message}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Typography component="h1" variant="h6" align="center">
-                        Jakiej wielkości pudełka potrzebujesz?
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                      <TextField
-                        fullWidth
-                        type="number"
-                        name="length"
-                        label="Length mm"
-                        id="length"
-                        inputProps={{ min: 60 }}
-                        {...register('length', {
-                          setValueAs: (v) => v === "" ? undefined : parseInt(v, 10),
-                        })}
-                        error={errors.length ? true : false}
-                      />
-                      <Typography variant="inherit" color="textSecondary">
-                        {errors.length?.message}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                      <TextField
-                        fullWidth
-                        type="number"
-                        name="width"
-                        label="Width mm"
-                        id="width"
-                        inputProps={{ min: 60 }}
-                        {...register('width', {
-                          setValueAs: (v) => v === "" ? undefined : parseInt(v, 10),
-                        })}
-                        error={errors.width ? true : false}
-                      />
-                      <Typography variant="inherit" color="textSecondary">
-                        {errors.width?.message}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                      <TextField
-                        fullWidth
-                        type="number"
-                        name="height"
-                        label="Height mm"
-                        id="height"
-                        inputProps={{ min: 30 }}
-                        {...register('height', {
-                          setValueAs: (v) => v === "" ? undefined : parseInt(v, 10),
-                        })}
-                        error={errors.height ? true : false}
-                      />
-                      <Typography variant="inherit" color="textSecondary">
-                        {errors.height?.message}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        fullWidth
-                        id="message"
-                        multiline
-                        label="Wiadomość"
-                        name="message"
-                        rows={2}
-                        {...register('message')}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      {/* <FormControlLabel
+          />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Box
+              sx={{
+                my: 4,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              {successMessage ?
+                (
+                  <Grid item xs={12}>
+                    <Typography component="h1" variant="h5">
+                      Dziękujemy!, wkrótce skontaktujemy się z Tobą.
+                    </Typography>
+                    <Button
+                      fullWidth
+                      variant="outlined"
+                      sx={{ mt: 3, mb: 2 }}
+                      onClick={resetForm}
+                    >
+                      Nowa wiadomość
+                    </Button>
+                  </Grid>
+                ) : (
+                  <Grid item xs={12}>
+                    <Typography component="h1" variant="h5" align="center">
+                      Napisz do nas:
+                    </Typography>
+                    <Box
+                      component="form"
+                      noValidate
+                      onSubmit={handleSubmit(onSubmit)}
+                      sx={{ mt: 3 }}
+                    >
+                      <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                          <TextField
+                            autoComplete="given-name"
+                            name="firstName"
+                            required
+                            fullWidth
+                            id="firstName"
+                            label="First Name"
+                            autoFocus
+                            {...register('firstName')}
+                            error={errors.firstName ? true : false}
+                          />
+                          <Typography variant="inherit" color="textSecondary">
+                            {errors.firstName?.message}
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                          <TextField
+                            required
+                            fullWidth
+                            id="lastName"
+                            label="Last Name"
+                            name="lastName"
+                            autoComplete="family-name"
+                            {...register('lastName')}
+                            error={errors.lastName ? true : false}
+                          />
+                          <Typography variant="inherit" color="textSecondary">
+                            {errors.lastName?.message}
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                          <TextField
+                            required
+                            fullWidth
+                            id="email"
+                            type="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            {...register('email')}
+                            error={errors.email ? true : false}
+                          />
+                          <Typography variant="inherit" color="textSecondary">
+                            {errors.email?.message}
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Typography component="h1" variant="h6" align="center">
+                            Jakiej wielkości pudełka potrzebujesz?
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                          <TextField
+                            fullWidth
+                            type="number"
+                            name="length"
+                            label="Length mm"
+                            id="length"
+                            inputProps={{ min: 60 }}
+                            {...register('length', {
+                              setValueAs: (v) => v === "" ? undefined : parseInt(v, 10),
+                            })}
+                            error={errors.length ? true : false}
+                          />
+                          <Typography variant="inherit" color="textSecondary">
+                            {errors.length?.message}
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                          <TextField
+                            fullWidth
+                            type="number"
+                            name="width"
+                            label="Width mm"
+                            id="width"
+                            inputProps={{ min: 60 }}
+                            {...register('width', {
+                              setValueAs: (v) => v === "" ? undefined : parseInt(v, 10),
+                            })}
+                            error={errors.width ? true : false}
+                          />
+                          <Typography variant="inherit" color="textSecondary">
+                            {errors.width?.message}
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                          <TextField
+                            fullWidth
+                            type="number"
+                            name="height"
+                            label="Height mm"
+                            id="height"
+                            inputProps={{ min: 30 }}
+                            {...register('height', {
+                              setValueAs: (v) => v === "" ? undefined : parseInt(v, 10),
+                            })}
+                            error={errors.height ? true : false}
+                          />
+                          <Typography variant="inherit" color="textSecondary">
+                            {errors.height?.message}
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                          <TextField
+                            fullWidth
+                            id="message"
+                            multiline
+                            label="Wiadomość"
+                            name="message"
+                            rows={3}
+                            {...register('message')}
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          {/* <FormControlLabel
                     control={
                       <Checkbox
                         value="allowExtraEmails"
@@ -302,7 +302,7 @@ export default function SignInSide() {
                     }
                     label=""
                   /> */}
-                      {/* <FormControlLabel
+                          {/* <FormControlLabel
                     control={
                       <Controller
                         name="allow"
@@ -318,36 +318,36 @@ export default function SignInSide() {
                     }
                     label="otrzym"
                   /> */}
-                      <Controller
-                        control={control}
-                        name="marketing"
-                        defaultValue={true}
-                        render={({ field: { value, onChange, ...field } }) => (
-                          <FormControlLabel
-                            control={
-                              <Checkbox onChange={onChange} checked={value} {...field} />
-                            }
-                            label="Chcę otrzymywać wiadomości o promocjach i nowościach na email"
+                          <Controller
+                            control={control}
+                            name="marketing"
+                            defaultValue={true}
+                            render={({ field: { value, onChange, ...field } }) => (
+                              <FormControlLabel
+                                control={
+                                  <Checkbox onChange={onChange} checked={value} {...field} />
+                                }
+                                label="Chcę otrzymywać wiadomości o promocjach i nowościach na email"
+                              />
+                            )}
                           />
-                        )}
-                      />
-                    </Grid>
-                  </Grid>
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                  >
-                    Wyślij
-                  </Button>
+                        </Grid>
+                      </Grid>
+                      <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                      >
+                        Wyślij
+                      </Button>
 
-                </Box></Grid>)}
-          </Box>
+                    </Box></Grid>)}
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
-    <Divider/>
+      </Box>
+      <Divider />
     </div>
   );
 }
