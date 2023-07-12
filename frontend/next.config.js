72b9ceb6cfe5c5c4821e40e73de3a956b4a6dcb3
Krzysztof Olszewski <hello@retryjoin.pl>
2023-07-12 09:05:47 +0200

    CORS fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -28,7 +28,7 @@ module.exports = withImages({
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
