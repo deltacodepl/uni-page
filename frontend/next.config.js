5a2b624c0442688c7b51af0a6aaf29bc72d47a2a
Krzysztof Olszewski <hello@retryjoin.pl>
2023-07-12 08:38:00 +0200

    CORS

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -24,9 +24,23 @@ module.exports = withImages({
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "api.achilles.pl" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   publicRuntimeConfig: {
-  // Will be available on both server and client
-  backendUrl: process.env.BACKEND_URL,
-}
+    // Will be available on both server and client
+    backendUrl: process.env.BACKEND_URL,
+  }
 });
 
