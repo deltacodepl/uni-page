a2ccc15e42035e53d5b89b9c8c5e9458d2a488c1
Krzysztof Olszewski <hello@retryjoin.pl>
2023-07-12 11:46:49 +0200

    backend url to https

--- backend/about/urls.py
+++ backend/about/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
+#from django.views.decorators.csrf import csrf_exempt
 from .views import AboutListView
 
 
 urlpatterns = [
-    path('', csrf_exempt(AboutListView.as_view())),
+    path('', AboutListView.as_view()),
 ]
