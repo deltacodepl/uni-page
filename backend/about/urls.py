a4a630a5a8c204c901a4753758c8ed556bdfb901
Krzysztof Olszewski <hello@retryjoin.pl>
2023-07-12 11:12:09 +0200

    django about csrf exempt

--- backend/about/urls.py
+++ backend/about/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from .views import AboutListView
 
+
 urlpatterns = [
-    path('', AboutListView.as_view()),
+    path('', csrf_exempt(AboutListView.as_view())),
 ]
